f7251a4bf89be7bc9e05f63a4d4f9e69195e0aee
EvangeliaS <evangelia.s@hotmail.gr>
2023-04-28 03:33:12 +0300

    tested given circuit

--- pytorch_imag.py
+++ pytorch_imag.py
@@ -129,24 +129,6 @@ def Gx(x):
         Gx[i] = torch.tensor(scipy.linalg.fractional_matrix_power(G[i], x)) #G to the power of x
     return Gx
 
-#x 10 random values between 0 and 2pi
-x = torch.rand(2, dtype=torch.float32)*2*np.pi
-print("x is: \n\n", x)
-
-#for multiple real x values test the results of Gx(x) and save them in Gx tensor in dim = 0 
-Gx_m = torch.zeros(11, 8, 8, dtype=torch.complex64)
-for i in range(x.size(dim = 0)):
-    print("xi is: \n\n", x[i].item())
-    Gx_i = Gx((x[i].item()))
-    print("Gx_i is: \n\n", Gx_i)
-    #Gx_m = torch.cat((Gx_m, Gx_i), 0) #append Gx_i to Gx
-    #for j in range(Gx_i.size(dim = 0)):
-        #print("Gx(x) is unitary: \n\n", torch.allclose(torch.eye(8, dtype = torch.complex64), Gx_i[j]@torch.conj(torch.transpose(Gx_i[j], 0,1))))
-        #print(Gx_i[j]@torch.conj(torch.transpose(Gx_i[j] ,0,1)))
-# print("Gx(x) is: \n\n", Gx_m[0], Gx_m[1])
-
-#save the results of G_i
-
 #create a loop that generates circuits with different x values and combination of Gx(x) gates 
 #and test the results of the circuits
 
@@ -171,5 +153,82 @@ for i in range(x.size(dim = 0)):
 
 # for vv3 in range(Gx_i.size(dim = 0)):
 #     for 2quit_gate in range(Gx_i)
+
+#_________________________________________________________________
+#x 10 random values between 0 and 2pi
+x = torch.rand(18, dtype=torch.float32)*2*np.pi
+print("x is: \n\n", x)
+Gm = []
+
+#for multiple real x values test the results of Gx(x) and save them in Gx tensor in dim = 0 
+for i in range(x.size(dim = 0)):
+    Gx_i = torch.zeros(11, 8, 8, dtype=torch.complex64)
+    print("xi is: \n\n", x[i].item())
+    Gx_i = Gx((x[i].item()))
+    Gm.append(Gx_i) #append in Gm list
+
+# for i in range(len(Gm)):
+#     print("Gm", i, "is: \n\n", Gm[i])
+    
+#create a specific circuit with specific gates and test the results
+#print("Gm len is: \n\n", len(Gm))
+i = 0
+while i in range(len(Gm)-1):
+    print("i is: \n\n", i)
+    G1 = Gm[i][5]  #get the first 2-qubit gate, 4-3-3
+    i+=1
+    G2 = Gm[i][1]   #get the second single qubit gate, 4-2-4
+    i+=1
+    #get the last single qubit gate 4-4-3
+    G3 = Gm[i][4]
+    i+=1
+    #1-1-4
+    G4 = Gm[i][7]
+    i+=1
+    #1-4-4
+    G5 = Gm[i][0]
+    i+=1
+    #4-3-4
+    G6 = Gm[i][2]
+    i+=1
+    #3-4-2
+    G7 = Gm[i][9]
+    i+=1
+    #4-4-3
+    G8 = Gm[i][4]
+    i+=1
+    #1-4-4
+    G9 = Gm[i][0]
+    i+=1
+    #4-1-3
+    G10 = Gm[i][6]
+    i+=1
+    #4-3-4
+    G11 = Gm[i][2]
+    i+=1
+    #4-4-3
+    G12 = Gm[i][4]
+    i+=1
+    #1-2-4
+    G13 = Gm[i][8]
+    i+=1
+    #1-4-4
+    G14 = Gm[i][0]
+    i+=1
+    #4-1-4
+    G15 = Gm[i][3]
+    i+=1
+    #1-4-3
+    G16 = Gm[i][10]
+    i+=1
+    #1-4-4
+    G17 = Gm[i][0]
+    i+=1
+    G18 = Gm[i][4] #4-4-3
+
+#multiply the 18 G matrices to get the final G matrix
+G_final = G1@G2@G3@G4@G5@G6@G7@G8@G9@G10@G11@G12@G13@G14@G15@G16@G17@G18
+
+print("G_final is: \n\n", G_final)
 
 
