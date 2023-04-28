e9aa280c020ac6a134847b7ac596049c042f4ee4
EvangeliaS <evangelia.s@hotmail.gr>
2023-04-28 03:42:11 +0300

    tested given circuit/beautified

--- pytorch_imag.py
+++ pytorch_imag.py
@@ -104,9 +104,6 @@ c11 = torch.kron(c11, ss3)
 #c1 - c5 are single qubit gates, c6 - c11 are two qubit gates
 vv3 = torch.stack((c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11))
 
-# print("vv3\n")
-# print(vv3)
-
 
 #Gi_, j_, k_, x_ := MatrixExp[I x KroneckerProduct[Assi, ssj, ssk]]
 G = torch.zeros(11, 8, 8, dtype=torch.complex64)
@@ -154,6 +151,7 @@ def Gx(x):
 # for vv3 in range(Gx_i.size(dim = 0)):
 #     for 2quit_gate in range(Gx_i)
 
+
 #_________________________________________________________________
 #x 10 random values between 0 and 2pi
 x = torch.rand(18, dtype=torch.float32)*2*np.pi
@@ -167,64 +165,69 @@ for i in range(x.size(dim = 0)):
     Gx_i = Gx((x[i].item()))
     Gm.append(Gx_i) #append in Gm list
 
-# for i in range(len(Gm)):
-#     print("Gm", i, "is: \n\n", Gm[i])
-    
+
+
 #create a specific circuit with specific gates and test the results
-#print("Gm len is: \n\n", len(Gm))
-i = 0
-while i in range(len(Gm)-1):
+for i in range(0, len(Gm)-1, 18):
     print("i is: \n\n", i)
-    G1 = Gm[i][5]  #get the first 2-qubit gate, 4-3-3
-    i+=1
-    G2 = Gm[i][1]   #get the second single qubit gate, 4-2-4
-    i+=1
-    #get the last single qubit gate 4-4-3
-    G3 = Gm[i][4]
-    i+=1
-    #1-1-4
-    G4 = Gm[i][7]
-    i+=1
-    #1-4-4
-    G5 = Gm[i][0]
-    i+=1
-    #4-3-4
-    G6 = Gm[i][2]
-    i+=1
-    #3-4-2
-    G7 = Gm[i][9]
-    i+=1
-    #4-4-3
-    G8 = Gm[i][4]
-    i+=1
-    #1-4-4
-    G9 = Gm[i][0]
-    i+=1
-    #4-1-3
-    G10 = Gm[i][6]
-    i+=1
-    #4-3-4
-    G11 = Gm[i][2]
-    i+=1
-    #4-4-3
-    G12 = Gm[i][4]
-    i+=1
-    #1-2-4
-    G13 = Gm[i][8]
-    i+=1
-    #1-4-4
-    G14 = Gm[i][0]
-    i+=1
-    #4-1-4
-    G15 = Gm[i][3]
-    i+=1
-    #1-4-3
-    G16 = Gm[i][10]
-    i+=1
-    #1-4-4
-    G17 = Gm[i][0]
-    i+=1
-    G18 = Gm[i][4] #4-4-3
+    G1 = Gm[i][5]   #get the first 2-qubit gate, 4-3-3
+    G2 = Gm[i+1][1] #get the second single qubit gate, 4-2-4
+    G3 = Gm[i+2][4] #get the last single qubit gate 4-4-3
+    G4 = Gm[i+3][7] #1-1-4
+    G5 = Gm[i+4][0] #1-4-4
+    G6 = Gm[i+5][2] #4-3-4
+    G7 = Gm[i+6][9] #3-4-2
+    G8 = Gm[i+7][4] #4-4-3
+    G9 = Gm[i+8][0] #1-4-4
+    G10 = Gm[i+9][6] #4-1-3
+    G11 = Gm[i+10][2] #4-3-4
+    G12 = Gm[i+11][4] #4-4-3
+    G13 = Gm[i+12][8] #1-2-4
+    G14 = Gm[i+13][0] #1-4-4
+    G15 = Gm[i+14][3] #4-1-4
+    G16 = Gm[i+15][10]#1-4-3
+    G17 = Gm[i+16][0] #1-4-4
+    G18 = Gm[i+17][4] #4-4-3
+
+#print("Gm len is: \n\n", len(Gm))
+# i = 0
+# while i in range(len(Gm)-1):
+#     print("i is: \n\n", i)
+#     G1 = Gm[i][5]  #get the first 2-qubit gate, 4-3-3
+#     i+=1
+#     G2 = Gm[i][1]   #get the second single qubit gate, 4-2-4
+#     i+=1
+#     G3 = Gm[i][4] #get the last single qubit gate 4-4-3
+#     i+=1
+#     G4 = Gm[i][7] #1-1-4
+#     i+=1
+#     G5 = Gm[i][0] #1-4-4
+#     i+=1 
+#     G6 = Gm[i][2]#4-3-4
+#     i+=1
+#     G7 = Gm[i][9] #3-4-2
+#     i+=1
+#     G8 = Gm[i][4]#4-4-3
+#     i+=1
+#     G9 = Gm[i][0]#1-4-4
+#     i+=1
+#     G10 = Gm[i][6]  #4-1-3
+#     i+=1
+#     G11 = Gm[i][2]    #4-3-4
+#     i+=1
+#     G12 = Gm[i][4] #4-4-3
+#     i+=1
+#     G13 = Gm[i][8] #1-2-4
+#     i+=1
+#     G14 = Gm[i][0] #1-4-4
+#     i+=1
+#     G15 = Gm[i][3]   #4-1-4
+#     i+=1
+#     G16 = Gm[i][10] #1-4-3
+#     i+=1
+#     G17 = Gm[i][0] #1-4-4
+#     i+=1
+#     G18 = Gm[i][4] #4-4-3
 
 #multiply the 18 G matrices to get the final G matrix
 G_final = G1@G2@G3@G4@G5@G6@G7@G8@G9@G10@G11@G12@G13@G14@G15@G16@G17@G18
