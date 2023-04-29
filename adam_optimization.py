5e4507d9faf13eaf1b8f0f7aa3aa37bebc74d3b2
EvangeliaS <evangelia.s@hotmail.gr>
2023-04-29 23:11:39 +0300

    tested learning rate scheduling in adam

--- adam_optimization.py
+++ adam_optimization.py
@@ -3,6 +3,7 @@ import torch.optim as optim
 import functions
 import numpy as np
 import pytorch_imag
+
 #define x tensor: 18 random values between 0 and 2pi
 x = torch.rand(18, dtype=torch.float32)*2*np.pi
 
@@ -19,6 +20,17 @@ for i in range(100):
     if i % 10 == 0:
         print(f"Iteration {i}: Cost {cost.item()}")
 
+#perform learning rate scheduling
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=0.0001, gamma=0.1)
+for i in range(100):
+    optimizer.zero_grad()
+    cost = functions.cost_function(G_final)
+    cost.backward()
+    optimizer.step()
+    scheduler.step()
+    if i % 10 == 0:
+        print(f"Iteration {i}: Cost {cost.item()}")
+
 # print the optimized G_final
 print("G_final after optimization: \n\n", G_final)
 print("Initial cost: \n\n", pytorch_imag.initial_cost)
@@ -27,3 +39,4 @@ print("Difference between initial and final cost: \n\n", pytorch_imag.initial_co
 
 
 #Note: the best result is about 0.2 reduction. We need to find a way to improve this result.
+#very improved results using the learning rate scheduling
