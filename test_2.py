bf7c91e7a1ccfb4c124de0a575e7c68bc713f890
EvangeliaS <evangelia.s@hotmail.gr>
2023-05-09 19:45:21 +0300

    defined model

--- test_2.py
+++ test_2.py
@@ -175,11 +175,11 @@ def generate_matrix(x_var):
 
 # define a function that generates a random value of x between 0 and 2pi
 def generate_x():
-    return torch.rand(18, dtype=torch.float32, requires_grad=True)* 2 * np.pi
+    return torch.rand(18, dtype=torch.float32, requires_grad=True)*100* 2 * np.pi
 
 # define your cost function
 def cost_function(G_final):
-    print(G_final@B)
+    #print(G_final@B)
     cost = 1 - 1/64*((torch.abs(torch.trace(G_final@B)))**2)
     return cost
 
@@ -210,19 +210,67 @@ def generate_optimal_matrix():
     return optimal_matrix
 
 # generate the optimal matrix
-optimal_matrix = generate_optimal_matrix()
+#optimal_matrix = generate_optimal_matrix()
 
 
-#test to check if the cost function is working on different values of x
+def trace(A):
+    return torch.sum(torch.diagonal(A))
 
-# for i in range(5):  # try 100 different random values of x
-#     x_var = torch.rand(18, dtype=torch.float32, requires_grad=True)* 2 * np.pi
+#define the model
+class MyModel(torch.nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.x = torch.nn.Parameter(torch.rand(18, dtype=torch.float32, requires_grad=True)* 2 * np.pi)
 
-#     print("x_var: ", x_var)
-#     Gm = []
+    def forward(self):
+        #A = generate_matrix(self.x)
+        Gm = []
+        # loop over the x values to generate the corresponding G matrices
+        for i in range(self.x.size(dim=0)):
+            Gx_i = torch.zeros(11, 8, 8, dtype=torch.complex64)
+            Gx_i = Gx(self.x[i].item())
+            Gm.append(Gx_i)
 
-#     G_final = generate_matrix(x_var)
+        for i in range(0, len(Gm), 18):
+            G1 = Gm[i][5]   #get the first 2-qubit gate(of the first x-modified Gx_i(i==0)), 4-3-3
+            G2 = Gm[i+1][1] #get the second single qubit gate, 4-2-4
+            G3 = Gm[i+2][4] #get the last single qubit gate 4-4-3
+            G4 = Gm[i+3][7] #1-1-4
+            G5 = Gm[i+4][0] #1-4-4
+            G6 = Gm[i+5][2] #4-3-4
+            G7 = Gm[i+6][9] #3-4-2
+            G8 = Gm[i+7][4] #4-4-3
+            G9 = Gm[i+8][0] #1-4-4
+            G10 = Gm[i+9][6] #4-1-3
+            G11 = Gm[i+10][2] #4-3-4
+            G12 = Gm[i+11][4] #4-4-3
+            G13 = Gm[i+12][8] #1-2-4
+            G14 = Gm[i+13][0] #1-4-4
+            G15 = Gm[i+14][3] #4-1-4
+            G16 = Gm[i+15][10]#1-4-3
+            G17 = Gm[i+16][0] #1-4-4
+            G18 = Gm[i+17][4] #4-4-3
 
-#     print("G_final: ", G_final.shape)
+            G_final = G1@G2@G3@G4@G5@G6@G7@G8@G9@G10@G11@G12@G13@G14@G15@G16@G17@G18
+        return G_final
+ 
+# initialize model and optimizer
+model = MyModel()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 
-#     print("cost is ", cost_function(G_final).item())
+# training loop
+num_epochs = 1000
+for i in range(num_epochs):
+    G_final = model()
+    loss = cost_function(G_final)
+    
+    # compute gradients and update parameters
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # print loss every 10 epochs
+    if i % 10 == 0:
+        #print learnable parameters
+        print("x: ", model.x)
+        print(f"Epoch {i}: loss={loss.item():.4f}")
