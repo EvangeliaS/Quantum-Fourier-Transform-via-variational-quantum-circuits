3837047980eb3ba7905b91192fbc5d38e8171691
EvangeliaS <evangelia.s@hotmail.gr>
2023-05-24 16:03:52 +0300

    x_new

--- functions.py
+++ functions.py
@@ -62,9 +62,6 @@ QF10 = torch.tensor(QF10)
 #make tensor with all the above tensors in it
 QF = torch.cat((QF3, QF4, QF5, QF6, QF7, QF8, QF9, QF10), 0)
 
-#find conjugate transpose of QF3
-B = torch.conj(torch.transpose(QF, 0,1))
-B.requires_grad = True
 
 #find kronecker products
 c1 = torch.kron(ss1, ss4)
@@ -119,44 +116,39 @@ for i in range(G.size(dim = 0)):
 #             sys.exit()
 
 
-# # define your cost function
-# def cost_function(G_final):
-#     cost = 1 - 1/64*((torch.abs(torch.trace(G_final@B)))**2)
-#     return cost
+# define a function that generates a random matrix with a given value of x
+def generate_matrix(x_var):
+    Gm = []
+    # loop over the x values to generate the corresponding G matrices
+    for i in range(x_var.size(dim=0)):
+        Gx_i = torch.zeros(11, 8, 8, dtype=torch.complex64)
+        Gx_i = Gx(x_var[i].item())
+        Gm.append(Gx_i)
 
-# # define a function that generates a random matrix with a given value of x
-# def generate_matrix(x_var):
-#     Gm = []
-#     # loop over the x values to generate the corresponding G matrices
-#     for i in range(x_var.size(dim=0)):
-#         Gx_i = torch.zeros(11, 8, 8, dtype=torch.complex64)
-#         Gx_i = Gx(x_var[i].item())
-#         Gm.append(Gx_i)
+    # multiply the 18 G matrices to get the final G matrix/circuit
+    #G_final = torch.eye(8, dtype=torch.complex64)
+    for i in range(0, len(Gm), 18):
+        G1 = Gm[i][5]   #get the first 2-qubit gate(of the first x-modified Gx_i(i==0)), 4-3-3
+        G2 = Gm[i+1][1] #get the second single qubit gate, 4-2-4
+        G3 = Gm[i+2][4] #get the last single qubit gate 4-4-3
+        G4 = Gm[i+3][7] #1-1-4
+        G5 = Gm[i+4][0] #1-4-4
+        G6 = Gm[i+5][2] #4-3-4
+        G7 = Gm[i+6][9] #3-4-2
+        G8 = Gm[i+7][4] #4-4-3
+        G9 = Gm[i+8][0] #1-4-4
+        G10 = Gm[i+9][6] #4-1-3
+        G11 = Gm[i+10][2] #4-3-4
+        G12 = Gm[i+11][4] #4-4-3
+        G13 = Gm[i+12][8] #1-2-4
+        G14 = Gm[i+13][0] #1-4-4
+        G15 = Gm[i+14][3] #4-1-4
+        G16 = Gm[i+15][10]#1-4-3
+        G17 = Gm[i+16][0] #1-4-4
+        G18 = Gm[i+17][4] #4-4-3
 
-#     # multiply the 18 G matrices to get the final G matrix/circuit
-#     #G_final = torch.eye(8, dtype=torch.complex64)
-#     for i in range(0, len(Gm), 18):
-#         G1 = Gm[i][5]   #get the first 2-qubit gate(of the first x-modified Gx_i(i==0)), 4-3-3
-#         G2 = Gm[i+1][1] #get the second single qubit gate, 4-2-4
-#         G3 = Gm[i+2][4] #get the last single qubit gate 4-4-3
-#         G4 = Gm[i+3][7] #1-1-4
-#         G5 = Gm[i+4][0] #1-4-4
-#         G6 = Gm[i+5][2] #4-3-4
-#         G7 = Gm[i+6][9] #3-4-2
-#         G8 = Gm[i+7][4] #4-4-3
-#         G9 = Gm[i+8][0] #1-4-4
-#         G10 = Gm[i+9][6] #4-1-3
-#         G11 = Gm[i+10][2] #4-3-4
-#         G12 = Gm[i+11][4] #4-4-3
-#         G13 = Gm[i+12][8] #1-2-4
-#         G14 = Gm[i+13][0] #1-4-4
-#         G15 = Gm[i+14][3] #4-1-4
-#         G16 = Gm[i+15][10]#1-4-3
-#         G17 = Gm[i+16][0] #1-4-4
-#         G18 = Gm[i+17][4] #4-4-3
-
-#         G_final = G1@G2@G3@G4@G5@G6@G7@G8@G9@G10@G11@G12@G13@G14@G15@G16@G17@G18
-#     return G_final
+        G_final = G1@G2@G3@G4@G5@G6@G7@G8@G9@G10@G11@G12@G13@G14@G15@G16@G17@G18
+    return G_final
 
 def Gx(x):
     Gx = torch.zeros(11, 8, 8, dtype=torch.complex64)
@@ -202,20 +194,69 @@ def matrix_exponential_gradient(A):
 
 # Example usage
 #x 10 random values between 0 and 2pi
+# x_var = torch.rand(18, dtype=torch.float32)*2*np.pi
+# print("x is: \n\n", x_var)
+# Gm = []
+
+# # loop over the x values to generate the corresponding G matrices
+# for i in range(x_var.size(dim=0)):
+#     Gx_i = torch.zeros(11, 8, 8, dtype=torch.complex64)
+#     Gx_i = Gx(x_var[i].item())
+#     Gm.append(Gx_i)
+
+# print(len(Gm))
+# print(Gm[0].size())
+# for i in range(len(Gm)-1):
+#     for j in range(Gm[i].size(dim = 0)):
+#         print(Gm[i][j])
+#         gradient = matrix_exponential_gradient(Gm[i][j])
+#         print("grad = ", gradient)
+
+#find conjugate transpose of QF3
+B = torch.conj(torch.transpose(QF, 0,1))
+B.requires_grad = True
+
+# Define your cost function
+def cost_function(x_var):
+    G_final = generate_matrix(x_var)
+    cost = 1 - 1/64 * ((torch.abs(torch.trace(G_final @ B)))**2)
+    return cost
+
+#create a function that changes the first value of the G_final matrix 
+#and then calculates the cost function
+
+
+def cost_function_1(x_var, gamma, delta):
+    print("x initial is: \n\n", x_var)
+    G_final = generate_matrix(x_var)
+    cost = 1 - 1/64 * ((torch.abs(torch.trace(G_final @ B)))**2)
+    print("cost initial = ", cost)
+    x_new = x_var.clone()
+
+    for i in range(len(x_var)):
+        #create a copy of the x_var tensor
+        x_var_sum = x_var.clone()
+        #change the first value of the x_var with a random value between 0 and 2pi
+        x_var_sum[i] = x_var[i] + delta
+        print("x sum is: \n\n", x_var_sum)
+        G_final_sum = generate_matrix(x_var_sum)
+        cost_sum = 1 - 1/64 * ((torch.abs(torch.trace(G_final_sum @ B)))**2)
+        print("cost sum = ", cost_sum)
+
+        x_var_diff = x_var.clone()
+        x_var_diff[i] = x_var[i] - delta
+        print("x diff is: \n\n", x_var_diff)
+        G_final_diff = generate_matrix(x_var_diff)
+        cost_diff = 1 - 1/64 * ((torch.abs(torch.trace(G_final_diff @ B)))**2)
+        print("cost diff = ", cost_diff)
+
+        x_new[i] = x_var[i] - gamma * ((cost_sum - cost_diff) / (2* delta))
+        ##print("x new is: \n\n", x_new)
+    return x_new
+
+
 x_var = torch.rand(18, dtype=torch.float32)*2*np.pi
-print("x is: \n\n", x_var)
-Gm = []
 
-# loop over the x values to generate the corresponding G matrices
-for i in range(x_var.size(dim=0)):
-    Gx_i = torch.zeros(11, 8, 8, dtype=torch.complex64)
-    Gx_i = Gx(x_var[i].item())
-    Gm.append(Gx_i)
+print(cost_function_1(x_var, 0.5, 0.05))
 
-print(len(Gm))
-print(Gm[0].size())
-for i in range(len(Gm)-1):
-    for j in range(Gm[i].size(dim = 0)):
-        print(Gm[i][j])
-        gradient = matrix_exponential_gradient(Gm[i][j])
-        print("grad = ", gradient)
+#define a 
